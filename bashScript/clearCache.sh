15d693397a7e18139eb8d7479f694d661d25466c
simitka <nikita@simitka.io>
2024-09-21 01:54:33 +0400

    [clearCache.sh] Add option to clear cache on all adb devices

--- bashScript/clearCache.sh
+++ bashScript/clearCache.sh
@@ -5,50 +5,64 @@ clear
 
 # Функция для вывода сообщения и выполнения команды farmx
 function finalize() {
-  echo "Чтобы продолжить нажмите любую кнопку..."
-  read -n 1
+  echo "Чтобы продолжить, нажмите любую кнопку..."
+  stty -icanon
+  dd bs=1 count=1 >/dev/null 2>&1
+  stty icanon
   farmx
-  exit $1
 }
 
-# Получаем список подключенных устройств
-devices=$(adb devices | awk 'NR>1 && $2=="device" {print $1}')
-device_count=$(echo "$devices" | wc -l)
+# Получаем список подключенных устройств, исключая пустые строки и строки "unauthorized", "offline" и т.д.
+devices=($(adb devices | awk 'NR>1 && $2=="device" {print $1}'))
+device_count=${#devices[@]}
 
 if [[ $device_count -eq 0 ]]; then
   echo "Нет подключенных adb устройств."
-  finalize 1
+  finalize
+  exit 1
 fi
 
 if [[ $device_count -eq 1 ]]; then
   # Если одно устройство
-  device=$(echo "$devices" | head -n 1)
+  device=${devices[1]}
   echo "Очищаем кэш приложения farm.parking.game для устройства $device..."
-  adb -s $device shell pm clear farm.parking.game
-  finalize 0
+  adb -s "$device" shell pm clear farm.parking.game
+  finalize
+  exit 0
 fi
 
 # Если несколько устройств
 echo "Подключено несколько устройств. Введи номер нужного:"
-i=1
-while IFS= read -r device; do
-  echo "    [$i] $device"
-  ((i++))
-done <<< "$devices"
+echo "    [0] Почистить кэш для всех устройств"
+
+for ((i = 0; i < device_count; i++)); do
+  echo "    [$((i + 1))] ${devices[i+1]}"
+done
 
 echo -n "Введите номер устройства для очистки кэша: "
 read device_choice
 
 # Проверка корректности ввода
-if ! [[ $device_choice =~ ^[0-9]+$ ]] || [[ $device_choice -lt 1 ]] || [[ $device_choice -gt $((i-1)) ]]; then
+if ! [[ $device_choice =~ ^[0-9]+$ ]] || [[ $device_choice -lt 0 ]] || [[ $device_choice -gt $device_count ]]; then
   echo
   echo "Некорректный выбор устройства."
-  finalize 1
+  finalize
+  exit 1
 fi
 
-# Получаем выбранное устройство
-selected_device=$(echo "$devices" | sed -n "${device_choice}p")
-echo
-echo "Очищаем кэш приложения farm.parking.game для устройства $selected_device..."
-adb -s $selected_device shell pm clear farm.parking.game
-finalize 0
+if [[ $device_choice -eq 0 ]]; then
+  # Очистка кэша для всех устройств
+  for device in "${devices[@]}"; do
+    echo "Очищаем кэш приложения farm.parking.game для устройства $device..."
+    adb -s "$device" shell pm clear farm.parking.game
+  done
+else
+  # Получаем выбранное устройство
+  selected_device=${devices[$((device_choice))]}
+  echo
+  echo "Очищаем кэш приложения farm.parking.game для устройства $selected_device..."
+  adb -s "$selected_device" shell pm clear farm.parking.game
+fi
+
+finalize
+exit 0
