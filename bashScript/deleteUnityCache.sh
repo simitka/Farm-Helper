e28b56dfd7a0fe246a197e008a4349741db55828
simitka <nikita@simitka.io>
2024-11-03 22:19:18 +0400

    [deleteUnityCache.sh] Added script for clearing application cache in Unity Editor

---
+++ bashScript/deleteUnityCache.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# Очищаем консоль
+clear
+
+# Функция для вывода сообщения и выполнения команды farmx
+function finalize() {
+    echo
+    echo "Чтобы продолжить, нажмите любую кнопку..."
+    stty -icanon
+    dd bs=1 count=1 >/dev/null 2>&1
+    stty icanon
+    farmx
+}
+
+# Удаляем папку ~/Library/Application Support/Liftapp
+rm -rf ~/Library/Application\ Support/Liftapp
+echo "Кэш farm.parking.game в Unity Editor'е удален."
+finalize
+exit 0
