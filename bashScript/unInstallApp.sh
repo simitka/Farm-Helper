90e7ee8ca952983d930defcc720e8612000b5309
simitka <nikita@simitka.io>
2024-11-03 20:38:55 +0400

    [unInstallApp.sh] Added script for uninstalling the application

---
+++ bashScript/unInstallApp.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+# Функция для вывода сообщения и выполнения команды farmx
+function finalize() {
+  echo
+  echo "Чтобы продолжить, нажмите любую кнопку..."
+  stty -icanon
+  dd bs=1 count=1 >/dev/null 2>&1
+  stty icanon
+  farmx
+}
+
+# Проверка наличия аргументов
+if [ "$#" -eq 0 ]; then
+  echo "Ошибка: в аргументе должно быть передано название устройства из ADB"
+  finalize
+  exit 1
+fi
+
+# Перебираем все аргументы
+for device in "$@"; do
+  # Пропускаем первый аргумент с индексом 0 (там $settings_file)
+  if (( index > 0 )); then
+  echo "Удаляем приложение farm.parking.game для устройства $device..."
+  adb -s "$device" uninstall farm.parking.game
+  fi
+  (( index++ ))
+done
+
+echo "Удаление приложения завершено."
+finalize
+exit 0
